d403f01252ca89539940bc31e3006c190a844fa2
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-27 05:13:07 -0300

    mutex

--- src/rinha_worker/mod.rs
+++ src/rinha_worker/mod.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use pingora::services::background::{BackgroundService, GenBackgroundService};
+use tokio::sync::Mutex;
 use tokio::sync::mpsc::Receiver;
 
 use async_trait::async_trait;
@@ -9,26 +10,28 @@ use pingora::server::ShutdownWatch;
 use crate::rinha_domain::Payment;
 
 pub struct RinhaWorker {
-    receiver: Receiver<Payment>,
+    receiver: Mutex<Receiver<Payment>>,
 }
 
 impl RinhaWorker {
     fn new(receiver: Receiver<Payment>) -> Self {
-        Self { receiver: receiver }
+        Self {
+            receiver: Mutex::new(receiver),
+        }
     }
 }
 
 #[async_trait]
 impl BackgroundService for RinhaWorker {
     async fn start(&self, mut shutdown: ShutdownWatch) {
-        let mut receiver = self.receiver;
-
         loop {
             tokio::select! {
                 _ = shutdown.changed() => {
                     break;
                 }
-                recv = receiver.recv() => {
+                recv = async {
+                    self.receiver.lock().await.recv().await
+                } => {
                     if let Some(payment) = recv {
                         dbg!(payment);
                     } else {
