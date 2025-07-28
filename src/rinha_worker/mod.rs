370e912bd03099164e1afb766b3130ccbbd8e1bf
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-28 14:56:36 -0300

    send

--- src/rinha_worker/mod.rs
+++ src/rinha_worker/mod.rs
@@ -1,13 +1,12 @@
 use crate::rinha_domain::Payment;
 use async_trait::async_trait;
-use http::header;
-use pingora::connectors::http::v1::Connector;
+use http::{Method, header};
+use pingora::connectors::http::Connector;
 use pingora::http::RequestHeader;
 use pingora::lb::LoadBalancer;
 use pingora::prelude::{HttpPeer, RoundRobin};
 use pingora::server::ShutdownWatch;
 use pingora::services::background::{BackgroundService, GenBackgroundService};
-use pingora::upstreams::peer::{PeerOptions, Scheme};
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio::sync::mpsc::Receiver;
@@ -30,36 +29,37 @@ impl RinhaWorker {
     async fn process_payment(&self, payment: Payment) {
         let load_balancer = Arc::clone(&self.load_balancer);
         let backend = load_balancer.select(b"", 8).unwrap();
-        let peer = HttpPeer {
-            _address: backend.addr.clone(),
-            sni: backend.addr.to_string(),
-            scheme: Scheme::HTTP,
-            proxy: None,
-            client_cert_key: None,
-            group_key: 0,
-            options: PeerOptions::new(),
-        };
+
+        let peer = HttpPeer::new(backend.addr.clone(), false, backend.addr.to_string());
+        dbg!(&peer);
         let connector = Connector::new(None);
         let (mut http, _) = connector.get_http_session(&peer).await.unwrap();
-        let mut request_header = RequestHeader::build("POST", b"/payments", None).unwrap();
-        let payment_serialized = &serde_json::ser::to_vec(&payment).unwrap()[..];
+
+        let payment = serde_json::ser::to_vec(&payment).unwrap();
+
+        let mut request_header = RequestHeader::build(Method::POST, b"/payments", None).unwrap();
+
         request_header
             .append_header(header::HOST, backend.addr.to_string())
             .unwrap();
         request_header
-            .append_header(header::CONTENT_LENGTH, payment_serialized.len())
+            .append_header(header::CONTENT_LENGTH, payment.len())
             .unwrap();
         request_header
             .append_header(header::CONTENT_TYPE, "application/json")
             .unwrap();
+
         http.write_request_header(Box::new(request_header))
             .await
             .unwrap();
-        http.write_body(payment_serialized).await.unwrap();
+        http.write_request_body(payment.into(), true).await.unwrap();
+        http.finish_request_body().await.unwrap();
 
-        let data = http.read_body_bytes().await.unwrap().unwrap();
+        dbg!(backend);
+        dbg!(http.read_response_header().await.unwrap());
 
-        dbg!(data);
+        // let data = http.read_body_bytes().await.unwrap().unwrap();
+        // dbg!(data);
     }
 }
 
