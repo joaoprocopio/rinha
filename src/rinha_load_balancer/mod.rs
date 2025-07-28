2d1d971594afe8880a69778fb10b5a679fd5b55d
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-28 04:28:53 -0300

    weighted

--- src/rinha_load_balancer/mod.rs
+++ src/rinha_load_balancer/mod.rs
@@ -1,10 +1,14 @@
+use futures::FutureExt;
 use http::{StatusCode, Uri};
 use pingora::{
     http::ResponseHeader,
-    lb::{Backend, LoadBalancer, health_check::HttpHealthCheck, selection::Consistent},
+    lb::{
+        Backend, Backends, LoadBalancer, discovery, health_check::HttpHealthCheck,
+        selection::Consistent,
+    },
     services::background::GenBackgroundService,
 };
-use std::{str::FromStr, sync::Arc, time::Duration};
+use std::{collections::BTreeSet, str::FromStr, sync::Arc, time::Duration};
 
 fn http_health_check() -> HttpHealthCheck {
     let mut health_checker = HttpHealthCheck::new("0.0.0.0", false);
@@ -27,10 +31,15 @@ fn http_health_check() -> HttpHealthCheck {
 }
 
 pub fn rinha_load_balancer_service() -> GenBackgroundService<LoadBalancer<Consistent>> {
-    let default_backend = Backend::new_with_weight("0.0.0.0:8001", 10).unwrap();
-    let fallback_backend = Backend::new_with_weight("0.0.0.0:8002", 1).unwrap();
-    let mut load_balancer =
-        LoadBalancer::<Consistent>::try_from_iter([default_backend, fallback_backend]).unwrap();
+    let discovery = discovery::Static::new(BTreeSet::from([
+        Backend::new_with_weight("0.0.0.0:8001", 10).unwrap(),
+        Backend::new_with_weight("0.0.0.0:8002", 1).unwrap(),
+    ]));
+    let backends = Backends::new(discovery);
+
+    let mut load_balancer = LoadBalancer::<Consistent>::from_backends(backends);
+
+    load_balancer.update().now_or_never().unwrap().unwrap();
 
     load_balancer.set_health_check(Box::new(http_health_check()));
     load_balancer.update_frequency = None;
