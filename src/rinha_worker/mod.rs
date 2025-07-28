526157235301fb53a28c218da4a7807c302794ff
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-28 06:03:49 -0300

    progresos

--- src/rinha_worker/mod.rs
+++ src/rinha_worker/mod.rs
@@ -1,9 +1,13 @@
 use crate::rinha_domain::Payment;
 use async_trait::async_trait;
+use http::header;
+use pingora::connectors::http::v1::Connector;
+use pingora::http::RequestHeader;
 use pingora::lb::LoadBalancer;
-use pingora::prelude::RoundRobin;
+use pingora::prelude::{HttpPeer, RoundRobin};
 use pingora::server::ShutdownWatch;
 use pingora::services::background::{BackgroundService, GenBackgroundService};
+use pingora::upstreams::peer::{PeerOptions, Scheme};
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio::sync::mpsc::Receiver;
@@ -25,8 +29,37 @@ impl RinhaWorker {
 impl RinhaWorker {
     async fn process_payment(&self, payment: Payment) {
         let load_balancer = Arc::clone(&self.load_balancer);
+        let backend = load_balancer.select(b"", 8).unwrap();
+        let peer = HttpPeer {
+            _address: backend.addr.clone(),
+            sni: backend.addr.to_string(),
+            scheme: Scheme::HTTP,
+            proxy: None,
+            client_cert_key: None,
+            group_key: 0,
+            options: PeerOptions::new(),
+        };
+        let connector = Connector::new(None);
+        let (mut http, _) = connector.get_http_session(&peer).await.unwrap();
+        let mut request_header = RequestHeader::build("POST", b"/payments", None).unwrap();
+        let payment_serialized = &serde_json::ser::to_vec(&payment).unwrap()[..];
+        request_header
+            .append_header(header::HOST, backend.addr.to_string())
+            .unwrap();
+        request_header
+            .append_header(header::CONTENT_LENGTH, payment_serialized.len())
+            .unwrap();
+        request_header
+            .append_header(header::CONTENT_TYPE, "application/json")
+            .unwrap();
+        http.write_request_header(Box::new(request_header))
+            .await
+            .unwrap();
+        http.write_body(payment_serialized).await.unwrap();
 
-        dbg!(payment, load_balancer.select(b"", 256));
+        let data = http.read_body_bytes().await.unwrap().unwrap();
+
+        dbg!(data);
     }
 }
 
--- todo.md
+++ todo.md
@@ -1,6 +1,6 @@
 # todo
 
 - [] remover todos os unwraps
-- [] ver se tá certo fazer o health check com o `Host: 1.1.1.1` header
+- [] trocar `RoundRobin` por um algoritmo que escolha sempre o backend healthy e default
 - [] guardar e retornar o summary quando pedido
 - [] trocar mpsc com aquela mutex maldita por watch channel
