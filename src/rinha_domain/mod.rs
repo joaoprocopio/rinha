6d7d4ae6d33042f06677ab257a7293c0efb586ef
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-28 15:12:05 -0300

    uuid

--- src/rinha_domain/mod.rs
+++ src/rinha_domain/mod.rs
@@ -1,8 +1,9 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Payment {
     #[serde(rename = "correlationId")]
-    pub correlation_id: String,
+    pub correlation_id: Uuid,
     pub amount: f32,
 }
