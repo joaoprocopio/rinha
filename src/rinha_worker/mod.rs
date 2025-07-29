4db37f74536bb104291907db02f5f0a80c9d3f60
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-28 23:30:42 -0300

    error handling

--- src/rinha_worker/mod.rs
+++ src/rinha_worker/mod.rs
@@ -8,6 +8,7 @@ use pingora::lb::LoadBalancer;
 use pingora::prelude::{HttpPeer, RoundRobin};
 use pingora::server::ShutdownWatch;
 use pingora::services::background::{BackgroundService, GenBackgroundService};
+use std::result;
 use std::sync::{Arc, LazyLock};
 use tokio::sync::RwLock;
 use tokio::sync::mpsc;
@@ -26,59 +27,8 @@ impl RinhaWorker {
         load_balancer: Arc<LoadBalancer<RoundRobin>>,
     ) -> Self {
         Self {
-            receiver: tokio::sync::RwLock::new(receiver),
+            receiver: RwLock::new(receiver),
             load_balancer: load_balancer,
-        }
-    }
-}
-
-impl RinhaWorker {
-    async fn process_payment(&self, payment: Payment) {
-        let load_balancer = Arc::clone(&self.load_balancer);
-        let backend = load_balancer.select(b"", 8).unwrap();
-        let target = backend.ext.get::<Target>().unwrap();
-
-        let peer = HttpPeer::new(backend.addr.clone(), false, backend.addr.to_string());
-        let connector = Connector::new(None);
-        let (mut http, _) = connector.get_http_session(&peer).await.unwrap();
-
-        let payment_ser = serde_json::ser::to_vec(&payment).unwrap();
-
-        let mut request_header = RequestHeader::build(Method::POST, b"/payments", None).unwrap();
-
-        request_header
-            .append_header(header::HOST, "0.0.0.0")
-            .unwrap();
-        request_header
-            .append_header(header::CONTENT_LENGTH, payment_ser.len())
-            .unwrap();
-        request_header
-            .append_header(header::CONTENT_TYPE, JSON_CONTENT_TYPE)
-            .unwrap();
-
-        http.write_request_header(Box::new(request_header))
-            .await
-            .unwrap();
-        http.write_request_body(payment_ser.into(), true)
-            .await
-            .unwrap();
-        http.finish_request_body().await.unwrap();
-        http.read_response_header().await.unwrap();
-
-        let response_header = http.response_header().unwrap();
-
-        match (target, response_header.status.is_success()) {
-            (Target::Default, true) => {
-                let mut counter = TARGET_COUNTER.write().await;
-                counter.default.requests += 1;
-                counter.default.amount += payment.amount;
-            }
-            (Target::Fallback, true) => {
-                let mut counter = TARGET_COUNTER.write().await;
-                counter.fallback.requests += 1;
-                counter.fallback.amount += payment.amount;
-            }
-            _ => (),
         }
     }
 }
@@ -94,10 +44,75 @@ impl BackgroundService for RinhaWorker {
                     break;
                 }
                 Some(payment) = receiver.recv() => {
-                    self.process_payment(payment).await
+                    process_payment(payment, Arc::clone(&self.load_balancer)).await
                 }
             }
         }
+    }
+}
+
+async fn process_payment(payment: Payment, load_balancer: Arc<LoadBalancer<RoundRobin>>) {
+    let backend = match load_balancer.select(b"", 8) {
+        Some(backend) => backend,
+        _ => return,
+    };
+    let target = match backend.ext.get::<Target>() {
+        Some(target) => target,
+        _ => return,
+    };
+
+    let peer = HttpPeer::new(backend.addr.clone(), false, backend.addr.to_string());
+    let connector = Connector::new(None);
+
+    let (mut http, _) = match connector.get_http_session(&peer).await {
+        Ok(session) => session,
+        _ => return,
+    };
+
+    let payment_ser = match serde_json::ser::to_vec(&payment) {
+        Ok(payment_ser) => payment_ser,
+        _ => return,
+    };
+
+    let mut request_header = match RequestHeader::build(Method::POST, b"/payments", None) {
+        Ok(request_header) => request_header,
+        _ => return,
+    };
+
+    if let Err(_) = request_header
+        .append_header(header::HOST, "0.0.0.0")
+        .and(request_header.append_header(header::CONTENT_LENGTH, payment_ser.len()))
+        .and(request_header.append_header(header::CONTENT_TYPE, JSON_CONTENT_TYPE))
+    {
+        return;
+    };
+
+    if let Err(_) = http
+        .write_request_header(Box::new(request_header))
+        .await
+        .and(http.write_request_body(payment_ser.into(), true).await)
+        .and(http.finish_request_body().await)
+        .and(http.read_response_header().await)
+    {
+        return;
+    };
+
+    let Some(response_header) = http.response_header() else {
+        return;
+    };
+
+    match (target, response_header.status.is_success()) {
+        (Target::Default, true) => {
+            let mut counter = TARGET_COUNTER.write().await;
+            counter.default.requests += 1;
+            counter.default.amount += payment.amount;
+        }
+        (Target::Fallback, true) => {
+            let mut counter = TARGET_COUNTER.write().await;
+            counter.fallback.requests += 1;
+            counter.fallback.amount += payment.amount;
+        }
+        _ => (),
     }
 }
 
