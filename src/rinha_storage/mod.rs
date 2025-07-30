b691ee07cac27ab5d9c6c6eff26bfb499ad41fa3
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-30 16:49:05 -0300

    vai

--- src/rinha_storage/mod.rs
+++ src/rinha_storage/mod.rs
@@ -1,6 +1,6 @@
-use fjall as storage;
-
 use crate::rinha_domain::Payment;
+use fjall as storage;
+use rand::Rng;
 
 impl From<&Payment> for storage::UserValue {
     fn from(value: &Payment) -> Self {
@@ -14,23 +14,45 @@ impl From<&storage::UserValue> for Payment {
     }
 }
 
+fn random_utc_datetime() -> chrono::DateTime<chrono::Utc> {
+    let mut rng = rand::rng();
+    let now = chrono::Utc::now();
+    let five_years_ago = now - chrono::Duration::days(365 * 5);
+    let offset = rng.random_range(five_years_ago.timestamp()..now.timestamp());
+    let rand_date = chrono::DateTime::from_timestamp(offset, 0).unwrap();
+
+    rand_date
+}
+
 pub fn setup() {
     let keyspace = storage::Config::new(".storage").open().unwrap();
     let items = keyspace
         .open_partition("payments", storage::PartitionCreateOptions::default())
         .unwrap();
 
-    let payment = serde_json::de::from_str::<Payment>(
-        "{\"correlationId\":\"0ef201ce-2995-4ec4-bf8b-3d03f8bfbff1\",\"amount\":19.90}",
-    )
-    .unwrap();
+    for _ in 1..=1000 {
+        let payment = Payment {
+            correlation_id: uuid::Uuid::new_v4(),
+            requested_at: random_utc_datetime(),
+            amount: 19.90,
+        };
 
-    items.insert("abc", &payment).unwrap();
+        items
+            .insert(payment.requested_at.timestamp().to_be_bytes(), &payment)
+            .unwrap();
+    }
 
-    let res = items.get("abc").unwrap().unwrap();
-    dbg!(&res);
+    for kv in items.range(
+        (chrono::Utc::now() - chrono::Duration::days(30))
+            .timestamp()
+            .to_be_bytes()..=chrono::Utc::now().timestamp().to_be_bytes(),
+    ) {
+        let kv = kv.unwrap();
+        let requested_at = &*kv.0;
+        let requested_at = i64::from_be_bytes(requested_at.try_into().unwrap());
+        let requested_at = chrono::DateTime::from_timestamp(requested_at, 0);
 
-    let res: Payment = (&res).into();
-
-    dbg!(&res);
+        let payment: Payment = (&kv.1).into();
+        dbg!(requested_at, payment);
+    }
 }
