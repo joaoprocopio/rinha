f2d3b720ef73d2eab1888f0fc6a50f6401ac8ecc
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-07-31 00:34:21 -0300

    vai

--- src/rinha_conf/mod.rs
+++ src/rinha_conf/mod.rs
@@ -1,5 +1,5 @@
 pub const RINHA_HOST: &str = "0.0.0.0";
 pub const RINHA_ADDR: &str = "0.0.0.0:9999";
 
-pub const RINHA_DEFAULT_BACKEND_ADDR: &str = "127.0.0.1:8001";
-pub const RINHA_FALLBACK_BACKEND_ADDR: &str = "127.0.0.1:8002";
+pub const RINHA_DEFAULT_BACKEND_ADDR: &str = "0.0.0.0:8001";
+pub const RINHA_FALLBACK_BACKEND_ADDR: &str = "0.0.0.0:8002";
