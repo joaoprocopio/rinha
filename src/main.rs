49c86203a7812bc2e6394e55700d0b861aae20bd
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-08-02 05:16:16 -0300

    worker funcional

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use crate::{rinha_core::Result, rinha_domain::Payment};
-use tokio::{
-    net::TcpListener,
-    sync::mpsc::{self, UnboundedReceiver, UnboundedSender},
-};
+use crate::rinha_core::Result;
+use tokio::net::TcpListener;
 
 mod rinha_chan;
 mod rinha_conf;
@@ -21,16 +18,21 @@ async fn main() -> Result<()> {
     let tcp_socket = rinha_net::create_tcp_socket(addr)?;
     let tcp_listener = TcpListener::from_std(tcp_socket.into())?;
 
-    tokio::spawn(async move {
-        let receiver = rinha_chan::get_receiver();
-        let mut receiver = receiver.lock().await;
-
-        loop {
-            let payment = receiver.recv().await;
-            tracing::debug!(?payment);
-        }
-    });
+    tokio::spawn(worker());
 
     let accept_loop = rinha_net::accept_loop(tcp_listener);
     tokio::spawn(accept_loop).await?
+}
+
+async fn worker() {
+    let receiver = rinha_chan::get_receiver();
+    let mut receiver = receiver.lock().await;
+
+    loop {
+        tokio::select! {
+            Some(payment) = receiver.recv() => {
+                dbg!(payment);
+            }
+        };
+    }
 }
