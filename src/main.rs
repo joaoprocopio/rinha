cfa9ee116df47f8ba7997fbc43b47d4dce2de16d
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-08-02 02:55:39 -0300

    vai

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,18 @@
-use crate::{rinha_core::Result, rinha_net::BoxBody};
-use http_body_util::{BodyExt, Empty, Full};
-use hyper::{
-    Method, Request, Response, StatusCode,
-    body::{Bytes, Incoming},
-    server::conn::http1,
-    service::service_fn,
-};
-use hyper_util::rt::TokioIo;
-use std::net::SocketAddr;
+use crate::rinha_core::Result;
 use tokio::net::TcpListener;
 
 mod rinha_conf;
 mod rinha_core;
 mod rinha_net;
 
-async fn router(req: Request<Incoming>) -> Result<Response<BoxBody>> {
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/") => {
-            let body = Full::new(Bytes::from("hello, world!"))
-                .map_err(|never| match never {})
-                .boxed();
-
-            Ok(Response::new(body))
-        }
-        _ => {
-            let body = Empty::new().map_err(|never| match never {}).boxed();
-
-            Ok(Response::builder()
-                .status(StatusCode::NOT_FOUND)
-                .body(body)
-                .unwrap())
-        }
-    }
-}
-
-async fn accept_loop(tcp_listener: TcpListener) -> Result<()> {
-    let mut http = http1::Builder::new();
-    http.pipeline_flush(true);
-
-    let service = service_fn(router);
-
-    loop {
-        let (tcp_stream, _) = tcp_listener.accept().await?;
-        let http = http.clone();
-
-        tokio::spawn(async move {
-            let io = TokioIo::new(tcp_stream);
-            if let Err(_) = http.serve_connection(io, service).await {};
-        });
-    }
-}
-
 #[tokio::main]
 async fn main() -> Result<()> {
     rinha_conf::bootstrap().await;
 
-    let addr: SocketAddr = rinha_conf::RINHA_ADDR.parse()?;
+    let addr = rinha_net::resolve_socket_addr(rinha_conf::RINHA_ADDR.as_str()).await?;
     let tcp_socket = rinha_net::create_tcp_socket(addr)?;
     let tcp_listener = TcpListener::from_std(tcp_socket.into())?;
 
-    let accept_loop = accept_loop(tcp_listener);
+    let accept_loop = rinha_net::accept_loop(tcp_listener);
     tokio::spawn(accept_loop).await?
 }
--- src/rinha_net/mod.rs
+++ src/rinha_net/mod.rs
@@ -1,11 +1,31 @@
+use crate::rinha_core::Result;
+use http_body_util::{BodyExt, Empty, Full};
+use hyper::{
+    Method, Request, Response, StatusCode,
+    body::{Bytes, Incoming},
+    server::conn::http1,
+    service::service_fn,
+};
+use hyper_util::rt::TokioIo;
 use socket2::{Domain, Protocol, SockAddr, Socket, Type};
 use std::net::SocketAddr;
-
-use crate::rinha_core::Result;
+use tokio::net::{TcpListener, ToSocketAddrs, lookup_host};
 
 pub type BoxBody = http_body_util::combinators::BoxBody<hyper::body::Bytes, hyper::Error>;
 
-/// Cria um socket reusável.
+// fn resolve_socket_addr(addr: &str) -> SocketAddr {
+//     let socket_addrs: Vec<std::net::SocketAddr> = addr.to_socket_addrs().unwrap().collect();
+
+//     SocketAddr::Inet(socket_addrs.into_iter().next().unwrap())
+// }
+
+pub async fn resolve_socket_addr<T: ToSocketAddrs>(addr: T) -> Result<SocketAddr> {
+    let mut addrs = lookup_host(addr).await?;
+    let addr = addrs.next().ok_or("Couldn't match an address")?;
+
+    Ok(addr)
+}
+
 pub fn create_tcp_socket(addr: SocketAddr) -> Result<Socket> {
     let domain = match addr {
         SocketAddr::V4(_) => Domain::IPV4,
@@ -21,4 +41,41 @@ pub fn create_tcp_socket(addr: SocketAddr) -> Result<Socket> {
     socket.listen(backlog)?;
 
     Ok(socket)
+}
+
+pub async fn router(req: Request<Incoming>) -> Result<Response<BoxBody>> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => {
+            let body = Full::new(Bytes::from("hello, world!"))
+                .map_err(|never| match never {})
+                .boxed();
+
+            Ok(Response::new(body))
+        }
+        _ => {
+            let body = Empty::new().map_err(|never| match never {}).boxed();
+
+            Ok(Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(body)
+                .unwrap())
+        }
+    }
+}
+
+pub async fn accept_loop(tcp_listener: TcpListener) -> Result<()> {
+    let mut http = http1::Builder::new();
+    http.pipeline_flush(true);
+
+    let service = service_fn(router);
+
+    loop {
+        let (tcp_stream, _) = tcp_listener.accept().await?;
+        let http = http.clone();
+
+        tokio::spawn(async move {
+            let io = TokioIo::new(tcp_stream);
+            if let Err(_) = http.serve_connection(io, service).await {};
+        });
+    }
 }
