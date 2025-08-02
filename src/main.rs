44ea988c9ca958bbc760201123a01879589f3e0f
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-08-02 00:55:59 -0300

    boxederr

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@ use hyper::{Request, Response, body::Bytes, server::conn::http1, service::servic
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
 
+type BoxedError = Box<dyn Error + Send + Sync>;
+type Result<T, E = BoxedError> = std::result::Result<T, E>;
+
 async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
     Ok(Response::new(Full::new(Bytes::from("Hello, world!"))))
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+async fn main() -> Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], 9999));
     let listener = TcpListener::bind(addr).await?;
 
