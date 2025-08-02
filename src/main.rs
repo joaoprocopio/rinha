dce265d7c64aae1b940fc5235ce8d1c4b7730dae
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-08-02 00:48:40 -0300

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,30 @@
+use std::{convert::Infallible, error::Error, net::SocketAddr};
+
+use http_body_util::Full;
+use hyper::{Request, Response, body::Bytes, server::conn::http1, service::service_fn};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
+async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello, world!"))))
+}
+
 #[tokio::main]
-async fn main() {
-    println!("abc");
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let addr = SocketAddr::from(([0, 0, 0, 0], 9999));
+    let listener = TcpListener::bind(addr).await?;
+
+    loop {
+        let (tcp, _) = listener.accept().await?;
+        let io = TokioIo::new(tcp);
+
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(io, service_fn(hello))
+                .await
+            {
+                println!("error serving connection {:?}", err)
+            }
+        });
+    }
 }
