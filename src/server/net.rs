385877ea2e07831202675a846a62df7ec9b7e360
joaoprocopio <joaovitorcprocopio@gmail.com>
2025-12-26 03:02:23 +0000

    cancel

--- src/server/net.rs
+++ src/server/net.rs
@@ -15,26 +15,32 @@ pub async fn run_worker(
     let mut receiver = server.receiver.lock().await;
 
     if let Some(uds_dirname) = server.env.uds_path.parent() {
+        fs::remove_dir_all(uds_dirname).await?;
         fs::create_dir_all(uds_dirname).await?;
     }
 
     let listener = UnixListener::bind(&server.env.uds_path)?;
 
     loop {
-        let (mut stream, _) = listener.accept().await?;
-
-        tokio::select! {
+        let mut stream = tokio::select! {
+            conn = listener.accept() => {
+                conn.and_then(|conn| Ok(conn.0))
+            }
             _ = &mut signal => {
-                tracing::info!("shutting down server worker");
-                stream.shutdown().await.unwrap_or_else(|err| {
-                    tracing::error!("uds socket shutdown failed with: {err}");
-                });
+                tracing::info!("gracefully shutting down uds stream listener");
                 break;
             }
+        }?;
+
+        tokio::select! {
             Some(bytes) = receiver.recv() => {
                 stream.write_all(&bytes).await.unwrap_or_else(|err|{
                     tracing::error!("writing to uds socket failed with: {err}");
                 });
+            }
+            _ = &mut signal => {
+                tracing::info!("gracefully shutting down channel received");
+                break;
             }
         }
     }
